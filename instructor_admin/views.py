2d6cd583610f6f9a2c6e6fc792b7f627e27a148e
Andrew Silvernail <andrew.silvernail@gmail.com>
2018-04-11 13:04:13 -0700

    * Wrapped post handling in a transaction so we can handle related objs. * Added basic exceptions for transaction rollbacks.

--- instructor_admin/views.py
+++ instructor_admin/views.py
@@ -4,8 +4,9 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView, View
 from django.db.models import ObjectDoesNotExist
 from text.models import Text, TextDifficulty
+from django.db import transaction
 
-from question.forms import QuestionForm, AnswerForm
+from question.forms import QuestionForm, AnswerForm, ModelForm
 from text.forms import TextForm
 
 
@@ -23,6 +24,26 @@ class AdminCreateQuizView(TemplateView):
     template_name = 'create_quiz.html'
 
 
+class ValidationException(Exception):
+    def __init__(self, errors : dict):
+        self.errors = errors
+
+
+class FormValidationException(Exception):
+    def __init__(self, form : ModelForm):
+        self.form = form
+        self.errors = self.form_validation_errors()
+
+    def form_validation_errors(self):
+        return {k: str(self.form.errors[k].data[0].message) for k in self.form.errors.keys()}
+
+
+class JSONException(Exception):
+    def __init__(self, json_str : str, json_decoder_exp: json.JSONDecodeError):
+        self.json = json_str
+        self.json_exp = json_decoder_exp
+
+
 class AdminAPIView(View):
     model = Text
 
@@ -35,54 +56,62 @@ class AdminAPIView(View):
             return HttpResponse(json.dumps(list(texts)))
 
     def post(self, request, *args, **kwargs):
-        def form_validation_errors(form):
-            return {"errors": {k: str(form.errors[k].data[0].message) for k in form.errors.keys()}}
-
-        text_params = json.loads(request.body.decode('utf8'))
-
-        # default difficulty
-        if 'difficulty' not in text_params or not text_params['difficulty']:
-            text_params['difficulty'] = 'intermediate_mid'
+        text = None
 
         try:
-            text_params['difficulty'] = TextDifficulty.objects.get(slug=text_params['difficulty']).pk
-        except ObjectDoesNotExist:
-            return HttpResponse(json.dumps({"errors": {
-                'difficulty': "text difficulty {0} does not exist".format(text_params['difficulty'])
-            }}), status=400)
+            with transaction.atomic():
+                try:
+                    text_params = json.loads(request.body.decode('utf8'))
+                except json.JSONDecodeError as e:
+                    raise JSONException(json_str=request.body.decode('utf8'), json_decoder_exp=e)
 
-        text_form = TextForm(text_params)
+                # default difficulty
+                if 'difficulty' not in text_params or not text_params['difficulty']:
+                    text_params['difficulty'] = 'intermediate_mid'
 
-        if not text_form.is_valid():
-            return HttpResponse(json.dumps(form_validation_errors(text_form)), status=400)
+                try:
+                    text_params['difficulty'] = TextDifficulty.objects.get(slug=text_params['difficulty']).pk
+                except ObjectDoesNotExist:
+                    raise ValidationException(
+                        errors={"errors": {'difficulty': "text difficulty {0} does not exist".format(
+                            text_params['difficulty'])
+                    }})
 
-        text = text_form.save()
+                text_form = TextForm(text_params)
 
-        for question_param in text_params['questions']:
-            question_param['text'] = text.pk
+                if not text_form.is_valid():
+                    raise FormValidationException(form=text_form)
 
-            # ignore 'order' param for now
-            question_param.pop('order')
-            # question_type -> type
-            question_param['type'] = question_param.pop('question_type')
+                text = text_form.save()
 
-            question_form = QuestionForm(question_param)
+                for question_param in text_params['questions']:
+                    question_param['text'] = text.pk
 
-            if not question_form.is_valid():
-                return HttpResponse(json.dumps(form_validation_errors(question_form)), status=400)
+                    # ignore 'order' param for now
+                    question_param.pop('order')
+                    # question_type -> type
+                    question_param['type'] = question_param.pop('question_type')
 
-            question = question_form.save()
+                    question_form = QuestionForm(question_param)
 
-            for answer_param in question_param['answers']:
-                answer_param['question'] = question.pk
-                # ignore 'order' param for now
-                answer_param.pop('order')
+                    if not question_form.is_valid():
+                        raise FormValidationException(form=question_form)
 
-                answer_form = AnswerForm(answer_param)
+                    question = question_form.save()
 
-                if not answer_form.is_valid():
-                    return HttpResponse(json.dumps(form_validation_errors(answer_form)), status=400)
+                    for answer_param in question_param['answers']:
+                        answer_param['question'] = question.pk
+                        # ignore 'order' param for now
+                        answer_param.pop('order')
 
-                answer = answer_form.save()
+                        answer_form = AnswerForm(answer_param)
+
+                        if not answer_form.is_valid():
+                            raise FormValidationException(form=answer_form)
+
+                        answer_form.save()
+
+        except (ValidationException, JSONException, FormValidationException) as exp:
+            return HttpResponse(json.dumps({"errors": exp.errors}), status=400)
 
         return HttpResponse(json.dumps({"id": text.pk}))
--- text/forms.py
+++ text/forms.py
@@ -6,4 +6,4 @@ from text.models import Text
 class TextForm(ModelForm):
     class Meta:
         model = Text
-        fields = ('source', 'difficulty', 'body', 'title',)
+        fields = ('source', 'difficulty', 'body', 'title', 'author',)
