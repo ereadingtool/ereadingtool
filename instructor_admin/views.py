98ec41a2a534b4f47d44c8ef992e3e4bca4f84c9
Andrew Silvernail <andrew.silvernail@gmail.com>
2018-05-29 11:29:07 -0700

    Quiz create page should return 404 for quizzes that do not exist.

--- instructor_admin/views.py
+++ instructor_admin/views.py
@@ -1,14 +1,15 @@
 import json
 
-from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
-from text.models import Text
-from user.views.mixin import ProfileView
-from mixins.view import ElmLoadJsView
 from csp.decorators import csp_replace
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
 
+from mixins.view import ElmLoadJsView
 from quiz.models import Quiz
+from user.views.mixin import ProfileView
 
 
 class AdminView(ProfileView, LoginRequiredMixin, TemplateView):
@@ -21,10 +22,16 @@ class QuizAdminView(AdminView):
 
 
 class AdminCreateEditQuizView(AdminView):
-    model = Text
+    model = Quiz
 
     fields = ('source', 'difficulty', 'body',)
     template_name = 'instructor_admin/create_edit_quiz.html'
+
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        if 'pk' in kwargs and not self.model.objects.filter(pk=kwargs['pk']):
+            raise Http404('quiz does not exist')
+
+        return super(AdminCreateEditQuizView, self).get(request, *args, **kwargs)
 
     # for CkEditor, allow exceptions to the CSP rules for unsafe-inline code and styles.
     @csp_replace(STYLE_SRC=("'self'", "'unsafe-inline'",), SCRIPT_SRC=("'self'", "'unsafe-inline'",))
