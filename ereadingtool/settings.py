818c56a25e813d5f46fca9f34d9f7f6c6a692adf
Andrew Silvernail <andrew.silvernail@gmail.com>
2018-10-12 14:16:12 -0700

    Remove old dev server IP.

--- ereadingtool/settings.py
+++ ereadingtool/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '198.199.110.226', '142.93.20.73']
+ALLOWED_HOSTS = ['0.0.0.0', '142.93.20.73']
 
 CSP_DEFAULT_SRC = ("'self'",)
 CSP_SCRIPT_SRC = ("'self'",)
