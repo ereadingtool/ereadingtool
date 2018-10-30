b8a3b5f8ebf98bb0f8c95f02ea9d2dfe0e32cf3d
Andrew Silvernail <andrew.silvernail@gmail.com>
2018-10-30 11:48:18 -0700

    Make create management command create definitions for a single section

--- text/management/commands/create_definitions.py
+++ text/management/commands/create_definitions.py
@@ -5,24 +5,17 @@ from text.consumers.instructor import ParseTextSectionForDefinitions
 
 
 class Command(BaseCommand):
-    help = 'Collects definitions for all text sections.'
+    help = 'Collects definitions for a particular text section.'
 
     def add_arguments(self, parser):
         parser.add_argument('text_section', action='store', help='Collects definitions for one text section.')
 
     def handle(self, *args, **options):
-        text_sections = []
-
         if options['text_section']:
             try:
                 text_section = TextSection.objects.get(pk=options['text_section'])
-                text_sections.append(text_section)
+                consumer = ParseTextSectionForDefinitions(scope={})
+
+                consumer.text_section_parse_word_definitions({'text_section_pk': text_section.pk})
             except TextSection.DoesNotExist:
                 raise CommandError(f"Can't find text section pk: {options['text_section']}")
-        else:
-            text_sections = TextSection.objects.all()
-
-        consumer = ParseTextSectionForDefinitions(scope={})
-
-        for section in text_sections:
-            consumer.text_section_parse_word_definitions({'text_section_pk': section.pk})
