640925529f7e84395b5f8912169f9022d9b7cdd3
Brian Ginsburg <brian@brianginsburg.com>
2020-07-21 07:53:50 -0700

    Add text modules
    Move TextReader and TextSearch files and directories into top level of SPA.
    Rename imports where needed.
    Install missing elm packages.

---
+++ web/src/TextReader/Text/Model.elm
@@ -0,0 +1,36 @@
+module TextReader.Text.Model exposing (..)
+
+import DateTime exposing (DateTime)
+
+
+type alias Text =
+    { id : Int
+    , title : String
+    , introduction : String
+    , author : String
+    , source : String
+    , difficulty : String
+    , conclusion : Maybe String
+    , created_by : Maybe String
+    , last_modified_by : Maybe String
+    , tags : Maybe (List String)
+    , created_dt : Maybe DateTime
+    , modified_dt : Maybe DateTime
+    }
+
+
+emptyText : Text
+emptyText =
+    { id = 0
+    , title = ""
+    , introduction = ""
+    , author = ""
+    , source = ""
+    , difficulty = ""
+    , conclusion = Nothing
+    , created_by = Nothing
+    , last_modified_by = Nothing
+    , tags = Nothing
+    , created_dt = Nothing
+    , modified_dt = Nothing
+    }
---
+++ web/src/TextReader/TextWord.elm
@@ -0,0 +1,113 @@
+module TextReader.TextWord exposing (..)
+
+import Dict exposing (Dict)
+import InstructorAdmin.Text.Translations exposing (..)
+import InstructorAdmin.Text.Translations.TextWord as TranslationsTextWord
+import InstructorAdmin.Text.Translations.Word.Kind as TranslationsWordKind
+
+
+type alias Translation =
+    { correct_for_context : Bool
+    , text : String
+    }
+
+
+type alias Translations =
+    List Translation
+
+
+type alias TextWordParams =
+    { id : Int
+    , instance : Int
+    , phrase : String
+    , grammemes : Maybe (List ( String, String ))
+    , translations : Maybe Translations
+    , word : ( String, Maybe TextGroupDetails )
+    }
+
+
+type TextWord
+    = TextWord Int Instance Phrase (Maybe Grammemes) (Maybe Translations) TranslationsWordKind.WordKind
+
+
+instance : TextWord -> Instance
+instance (TextWord _ inst _ _ _ _) =
+    inst
+
+
+phrase : TextWord -> Phrase
+phrase (TextWord _ _ phr _ _ _) =
+    phr
+
+
+word : TextWord -> TranslationsWordKind.WordKind
+word (TextWord _ _ _ _ _ word_kind) =
+    word_kind
+
+
+wordType : TextWord -> String
+wordType text_word =
+    TranslationsTextWord.wordTypeToString (word text_word)
+
+
+group : TextWord -> Maybe TextGroupDetails
+group (TextWord _ _ _ _ _ w) =
+    TranslationsTextWord.wordKindToGroup w
+
+
+grammemes : TextWord -> Maybe Grammemes
+grammemes (TextWord _ _ _ grams _ _) =
+    grams
+
+
+grammemesToString : TextWord -> String
+grammemesToString text_word =
+    case grammemes text_word of
+        Just grs ->
+            String.join ", " <|
+                List.map (\( g, v ) -> g ++ ": " ++ v) <|
+                    Dict.toList grs
+
+        Nothing ->
+            ""
+
+
+hasTranslations : TextWord -> Bool
+hasTranslations text_word =
+    case translations text_word of
+        Just trs ->
+            True
+
+        Nothing ->
+            False
+
+
+translations : TextWord -> Maybe Translations
+translations (TextWord _ _ _ _ trans _) =
+    trans
+
+
+new : Int -> Instance -> Phrase -> Maybe Grammemes -> Maybe Translations -> TranslationsWordKind.WordKind -> TextWord
+new id inst phr grams trans w =
+    TextWord id inst phr grams trans w
+
+
+newGrammemeFromList : Maybe (List ( String, String )) -> Grammemes
+newGrammemeFromList grams =
+    case grams of
+        Just grs ->
+            Dict.fromList grs
+
+        Nothing ->
+            Dict.empty
+
+
+newFromParams : TextWordParams -> TextWord
+newFromParams params =
+    TextWord
+        params.id
+        params.instance
+        params.phrase
+        (Just (newGrammemeFromList params.grammemes))
+        params.translations
+        (TranslationsTextWord.strToWordType params.word)
