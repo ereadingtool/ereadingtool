eb618eee7f5e879ea6869a472b28b350082b67c6
dejef <dejef@pdx.edu>
2020-11-04 16:03:34 -0800

    Merge conflict
    Seems that I forgot about the decorator factory. Rather than go with the
    previous implementation (on upgrade/staging), we want to keep all of the
    code on upgrade/jwt_auth. Unsure as to why a merge conflict was created
    in the first place.

--- auth/normal_auth.py
+++ auth/normal_auth.py
@@ -7,7 +7,6 @@ from user.instructor.models import Instructor
 from django.http import HttpResponse
 from urllib.parse import parse_qs
 
-<<<<<<< Updated upstream
 def jwt_valid(status: int, errors: Dict):
     def decorator(func):
         def validate(*args, **kwargs):
@@ -23,17 +22,6 @@ def jwt_valid(status: int, errors: Dict):
                 jwt_encoded = meta['HTTP_AUTHORIZATION'][7:] if meta['HTTP_AUTHORIZATION'][:7] == 'Bearer ' else meta['HTTP_AUTHORIZATION']
             try:
                 jwt_decoded = jwt.decode(jwt_encoded, secret_key, algorithms=['HS256'])
-=======
-def jwt_validation(scope):
-    """ Take JWT from query string to check the user against the db and validate its timestamp """
-    if not scope and not scope['query_string']:
-        return None
-    else:
-        secret_key = os.getenv('DJANGO_SECRET_KEY')
-        try:
-            qs = scope['query_string'][6:] if scope['query_string'][:6] == b'token=' else scope['query_string']
-            jwt_decoded = jwt.decode(qs, secret_key, algorithms=['HS256'])
->>>>>>> Stashed changes
 
                 if jwt_decoded['exp'] <= time.time():
                     # then their token has expired 
