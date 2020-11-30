63445631ad884b6d1539205f9e8264f7d061fc95
dejef <dejef@pdx.edu>
2020-11-30 14:47:29 -0800

    Possible fix identified
    Would like to return the `number_of_sections` if it turns out the reader
    has already completed the text. Oddly enough, we store this multiple
    times in the database, which would lead to the
    `text_reading_studenttextreading` table possibly containing a copy of
    `intro`, `in_progess`, and `completed`. At this time Ive only seen a
    single `intro` or `in_progess`, but it looks like many `completed` are
    allowed. I find this bizarre, the table should be no larger than
    `student_id` X `text_id` in my opinion. Why would we keep track of their
    previous completions, especially if we're looking to make their first
    answer history.

--- user/mixins/models.py
+++ user/mixins/models.py
@@ -52,7 +52,7 @@ class TextReadings(models.Model):
             last_read_dt = last_reading.last_read_dt.isoformat()
 
         return last_read_dt
-    # TODO: questions correct bug is here?
+    # TODO: questions correct feature is here?
     def last_read_questions_correct(self, text: Text) -> Optional[Tuple[int, int]]:
         last_read = self.last_read(text)
 
@@ -72,13 +72,23 @@ class TextReadings(models.Model):
         return last_read
 
     def sections_complete_for(self, text: Text) -> int:
+        """
+        Ideally we'd just check to see if something has been completed. If so, the sections_complete would be
+        the same as the number of sections in the text. Unfortunately, if we try to `.get(text=text)` an exception
+        occurs because we have multiple entries with the state `complete`.
+        """
         sections_complete = 0
 
+        # Sections complete bug here
         if self.text_readings.exclude(state=TextReadingStateMachine.complete.name).filter(text=text).exists():
             current_text_reading = self.text_readings.exclude(
                 state=TextReadingStateMachine.complete.name).get(text=text)
 
             if not current_text_reading.state_machine.is_intro:
                 sections_complete = current_text_reading.current_section.order
+        # TODO test on texts that have never been read
+        else:
+            current_text_reading = self.text_readings.get(text=text)
+            sections_complete = current_text_reading.number_of_sections
 
         return sections_complete
