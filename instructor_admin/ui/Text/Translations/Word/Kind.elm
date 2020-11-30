90740068f8f6cd2bc629666beccf962c4f762339
Brian Ginsburg <brian@brianginsburg.com>
2020-11-30 13:48:32 -0800

    Remove original Elm modules from original app
    All modules have been brought into the SPA architecture in the web/ directory.
    These modules were used in the server-rendered version of the app, and are no
    longer used in the new version of the app.

--- instructor_admin/ui/Text/Translations/Word/Kind.elm
+++
@@ -1,8 +0,0 @@
-module Text.Translations.Word.Kind exposing (WordKind(..))
-
-import Text.Translations exposing (TextGroupDetails)
-
-
-type WordKind
-    = SingleWord (Maybe TextGroupDetails)
-    | CompoundWord
