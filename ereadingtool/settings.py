03f42d815a14252311dab3a98e818ee3270fcee3
dejef <dejef@pdx.edu>
2021-02-11 10:25:48 -0800

    Adding to ALLOWED_HOST for second dev server

--- ereadingtool/settings.py
+++ ereadingtool/settings.py
@@ -114,7 +114,8 @@ ALLOWED_HOSTS = ['0.0.0.0',
                  'stepstoadvancedreading.org',
                  'steps2advancedreading.org',
                  'steps2ar.org',
-                 'api.steps2ar.org']
+                 'api.steps2ar.org',
+                 'delamare.xyz']
 
 CSP_DEFAULT_SRC = ("'self'",)
 CSP_SCRIPT_SRC = ("'self'",)
