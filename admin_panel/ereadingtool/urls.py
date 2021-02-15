4d34e688b9c1d5c1745f2732ddddedc408118420
dejef <dejef@pdx.edu>
2021-02-14 18:48:08 -0800

    admin URL is only subdomain

--- admin_panel/ereadingtool/urls.py
+++ admin_panel/ereadingtool/urls.py
@@ -42,7 +42,7 @@ urlpatterns = [
 
 #    path('admin/', include('instructor_admin.urls')),
 
-    path('django-admin/', admin.site.urls),
+    path('', admin.site.urls),
 
 #    path('', RedirectView.as_view(url=reverse_lazy('student-login'))),
 #    path('error', TemplateView.as_view(template_name='error_page.html'), name='error-page'),
