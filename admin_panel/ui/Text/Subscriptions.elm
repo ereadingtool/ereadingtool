a9cddd6a7229d0c2d9e568fe8ffb3b2afd9af9a6
dejef <dejef@pdx.edu>
2021-02-17 15:56:21 -0800

    DELETED ALL STATIC FILES
    Pehaps this should be on its own branch, but we're expirimenting with a
    second dev server at the moment. Trying to reconfigure the static files
    being shared by django so that we can get the admin panel working.
    
    Currently everything is brought the users with Parcel. We still need to
    serve CSS out of the admin panel's container, preferably by way of
    WhiteNoise. We'll see how it goes.

--- admin_panel/ui/Text/Subscriptions.elm
+++
@@ -1,9 +0,0 @@
-module Text.Subscriptions exposing (..)
-
-import Ports exposing (selectAllInputText, ckEditor, ckEditorUpdate)
-import Text.Update exposing (Msg)
-import Text.Component as Text exposing (TextComponent)
-
-subscriptions : (Msg -> msg) -> { a | text_component: TextComponent} -> Sub msg
-subscriptions msg model =
-  ckEditorUpdate (Text.Update.UpdateTextBody >> msg)
